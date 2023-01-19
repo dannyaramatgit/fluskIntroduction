ea51de38ee34579736bea46d42e577905c0c3b1d
Danny Aram <dannyaram@gmail.com>
2023-01-19 14:32:47 +0200

    studend add and list with db

--- contact.py
+++ contact.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request, flash
 from forms import ContactForm
+from flask_wtf.csrf import CSRFProtect
+import sqlite3 as sql
+
 app = Flask(__name__)
+
+# Add this somewhere in your app's initialization
+csrf = CSRFProtect(app)
 app.secret_key = 'development key'
+csrf.init_app(app)
 
 @app.route('/contact', methods = ['GET', 'POST'])
 def contact():
@@ -9,12 +16,54 @@ def contact():
    
    if request.method == 'POST':
       if form.validate() == False:
+         print(form.errors) 
          flash('All fields are required.')
          return render_template('contact.html', form = form)
       else:
-         return render_template('success.html')
+         return 'success'
    elif request.method == 'GET':
       return render_template('contact.html', form = form)
 
+
+@app.route('/enternew')
+def new_student():
+   return render_template('student.html')
+
+@app.route('/list')
+def list():
+   con = sql.connect("mydb.db")
+   con.row_factory = sql.Row
+   
+   cur = con.cursor()
+   cur.execute("select * from students")
+   
+   rows = cur.fetchall(); 
+   return render_template("list.html",rows = rows)
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+   if request.method == 'POST':
+      try:
+         nm = request.form['nm']
+         addr = request.form['add']
+         city = request.form['city']
+         pin = request.form['pin']
+         
+         with sql.connect("mydb.db") as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO students (name,addr,city,pin) VALUES (?,?,?,?)",(nm,addr,city,pin) )
+            
+            con.commit()
+            msg = "Record successfully added"
+      except:
+         con.rollback()
+         msg = "error in insert operation"
+      
+      finally:
+         return render_template("student_result.html",msg = msg)
+         con.close()
+
+
 if __name__ == '__main__':
    app.run(debug = True)
+
--- forms.py
+++ forms.py
@@ -1,15 +1,14 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField,RadioField,TextAreaField,IntegerField,SelectField,SubmitField
 from wtforms import validators, ValidationError
 from wtforms.validators import DataRequired,Email
 
-class ContactForm(Form):
-   name = StringField("Name Of Student",[validators.DataRequired("Please enter your name.")])
+class ContactForm(FlaskForm):
+   name = StringField("Name Of Student",validators=[validators.DataRequired("Name is required")])
    Gender = RadioField('Gender', choices = [('M','Male'),('F','Female')])
    Address = TextAreaField("Address")
    
-   email = StringField("Email",[validators.DataRequired("Please enter your email address."),
-      Email("Please enter your email address.")])
+   email = StringField("Email", validators=[validators.DataRequired("email required"),validators.Email("Bad email")])
    
    Age = IntegerField("age")
    language = SelectField('Languages', choices = [('cpp', 'C++'), 
